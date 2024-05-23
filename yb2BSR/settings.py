ab2ae262e60f2c8b2a9bb8fca26cfa06d4a12180
dugengmaster <dugengmaster@gmail.com>
2024-05-23 21:35:28 +0800

    hidden django secret key

--- yb2BSR/settings.py
+++ yb2BSR/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-*9=2+w6166u8c54ei-ov#i5j3w2#qh5jm%%97vhctat9%w(o8("
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # Environment variable: ACCESS_TOKEN
 env_path = os.path.join(BASE_DIR, ".env")
